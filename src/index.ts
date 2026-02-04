0714b89ac3e837d743c27ba24eeac98edac1fcc9
Larsundso <Larsundso3@gmail.com>
2026-02-04 16:17:28 +0100

    feat: add getPathFromError function to extract file path from error stack trace

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ export * from './Types/Redis.js';
 //#region Functions
 export { default as getChannelPerms } from './Functions/getChannelPerms.js';
 export { default as getGuildPerms } from './Functions/getGuildPerms.js';
+export { default as getPathFromError } from './Functions/getPathFromError.js';
 export { default as getRandom } from './Functions/getRandom.js';
 export { deserialize, serialize } from './Serialization.js';
 //#endregion
